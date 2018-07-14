82e704d70198ba9e990c3b7522cf3769bc62bf45
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-07-14 17:17:38 +0000

    automatically backed up by learn

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const Filter = ({filters, handleChange}) => 
+const Filter = ({filters, handleChange}) =>
 <select onChange={handleChange} defaultValue='all'>
   <option value='all'>All</option>
-  {filters.map(filter => 
+  {filters.map(filter =>
   <option key={filter} value={filter}>{filter}</option>
-)}
+  )}
+  </select>
+
+  
 export default Filter;
