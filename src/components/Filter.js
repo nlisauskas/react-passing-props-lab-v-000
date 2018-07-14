7d36057b85a218949bcfca2e50dc87e1a02792cf
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-07-14 17:27:30 +0000

    Done.

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,34 +1,19 @@
-import React, { Component } from 'react';
 
-class Filter extends Component {
-  constructor() {
-    super();
+import React from 'react';
 
-    this.state = {
-      filters: []
-    };
-  }
-
-  componentWillMount() {
-    this.fetchFilters();
-  }
-
-  fetchFilters = () => {
-    fetch('/api/fruit_types')
-      .then(response => response.json())
-      .then(filters => this.setState({ filters }));
-  }
-
-  render() {
-    return (
-      <select onChange={this.props.handleChange} defaultValue='all'>
+      const Filter = ({filters, handleChange}) =>
+      <select onChange={handleChange} defaultValue='all'>
         <option value='all'>All</option>
-        {this.state.filters.map(filter =>
+        {filters.map(filter =>
           <option key={filter} value={filter}>{filter}</option>
         )}
       </select>
-    );
-  }
+
+
+
+Filter.defaultProps = {
+  filters: [],
+  handleChange: function() {}
 }
 
 export default Filter;
